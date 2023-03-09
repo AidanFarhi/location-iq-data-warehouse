8be23b5a159d2e38164549c2c40df056d99e06d1
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-03-09 16:11:55 -0500

    added create table statements

---
+++ CreateTableStatements/annual_salary.sql
@@ -0,0 +1,6 @@
+CREATE TABLE annual_salary (
+    county VARCHAR(100),
+    occupation VARCHAR(100),
+    salary FLOAT,
+    as_of_date DATE
+)
---
+++ CreateTableStatements/household.sql
@@ -0,0 +1,6 @@
+CREATE TABLE household (
+    household_id INTEGER,
+    adults INTEGER,
+    children INTEGER,
+    working_adults INTEGER
+)
