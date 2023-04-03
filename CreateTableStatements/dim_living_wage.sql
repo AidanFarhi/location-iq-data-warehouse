be5ed6a27ed963e56649dfa2202e3c0eb68a81ee
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-04-03 14:32:40 -0400

    added create table statement for dim_living_wage table

---
+++ CreateTableStatements/dim_living_wage.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dim_living_wage (
+    location_id INTEGER,
+    hourly_wage FLOAT,
+    number_of_adults INTEGER,
+    number_of_children INTEGER,
+    number_of_working_adults INTEGER,
+    snapshot_date DATE
+)
--- CreateTableStatements/wage.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE wage (
-    county VARCHAR(100),
-    hourly_wage FLOAT,
-    wage_level VARCHAR(100),
-    as_of_date DATE,
-    household_id INTEGER
-)
