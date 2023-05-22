c11b71ed781c2d5db404f109128fee17d41dc57b
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-05-21 22:04:22 -0400

    added fact_crime_rate create table statement

--- CreateTableStatements/dim_crime_rate.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dim_crime_rate (
-    location_id INTEGER,
-    crime_type VARCHAR(100),
-    rate FLOAT,
-    snapshot_date DATE
-)
---
+++ CreateTableStatements/fact_crime_rate.sql
@@ -0,0 +1,6 @@
+CREATE TABLE fact_crime_rate (
+    location_id INTEGER,
+    snapshot_date_id INTEGER,
+    crime_type VARCHAR(100),
+    rate FLOAT
+)
