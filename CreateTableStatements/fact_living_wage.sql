fdfe0fe7a5800ca4ad85d651d019f246121660d6
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-05-24 21:35:58 -0400

    changed living_wage from a dimension to a fact

--- CreateTableStatements/fact_living_wage.sql
+++ CreateTableStatements/fact_living_wage.sql
@@ -1,8 +1,8 @@
-CREATE TABLE dim_living_wage (
+CREATE TABLE fact_living_wage (
     location_id INTEGER,
+    snapshot_date_id INTEGER,
     hourly_wage FLOAT,
     number_of_adults INTEGER,
     number_of_children INTEGER,
-    number_of_working_adults INTEGER,
-    snapshot_date DATE
+    number_of_working_adults INTEGER
 )
