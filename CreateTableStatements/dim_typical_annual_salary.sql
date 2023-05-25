aa87d614657368ac6fac1abefafc1da6717f9076
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-05-24 21:33:05 -0400

    changed typical_annual_salary from dimension to fact table

--- CreateTableStatements/dim_typical_annual_salary.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE dim_living_wage (
-    location_id INTEGER,
-    hourly_wage FLOAT,
-    number_of_adults INTEGER,
-    number_of_children INTEGER,
-    number_of_working_adults INTEGER,
-    snapshot_date DATE
-)
---
+++ CreateTableStatements/fact_typical_annual_salary.sql
@@ -0,0 +1,6 @@
+CREATE TABLE fact_typical_annual_salary (
+    location_id INTEGER,
+    snapshot_date_id INTEGER,
+    occupation VARCHAR(100),
+    salary FLOAT
+)
